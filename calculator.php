4915e55afc984437715b6c15f3c1b1f612b61827
Gabriel Freitas <132530657+gabrielfnaut@users.noreply.github.com>
2023-08-01 21:55:01 -0300

    Update calculator.php

--- calculator.php
+++ calculator.php
@@ -1,10 +1,19 @@
 <?php
 
-// + - * /
+/* 
++ (adição)
+- (subtração)
+* (multiplicação)
+/ (divisão)
+** (exponenciação)
+numpar (núemero par) % 
+perc (percentual desconto) 
+perc+ (percentual acrécimo)
+*/
 
 $value = "4";
-$value2 = "4";
-$operation = "+";
+$value2 = "2";
+$operation = "numpar";
 $result = 0 ;
 
 if ($operation == "+") {  
@@ -27,16 +36,40 @@ if ($operation == "**" ) {
     $result = $value ** $value2 ;
 }
 
-if ($operation == "%" ) {
-    $result = $value % $value2 ;
+if ($operation == "numpar" ) {
+    $par = $value % 2 ; // 0 || 1...
+    $par2 = $value2 % 2 ;
+    $saopares = ($par == 0 && $par2 == 0) ; // true || false
+
+if ($saopares) {
+    echo "Os valores informados sãp pares.  ";
+}
+}  
+
+
+if ($operation == "perc") {
+    $result = $value - ($value * $value2) / 100 ;   
 }
 
-// verificar se um número é par (% )
+if ($operation == "perc+") {
+    $result = ($value * $value2) / 100 ; 
+}
 
 
-    
+
+
+if ($operation == "mmc");
+
+
+
+
+
 
 echo $result;
+
+
+
+
 
 
 
