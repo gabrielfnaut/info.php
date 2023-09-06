f65613cf46f317c4ad3f14b4bbe0e2f48415eb2f
gabrielfnaut <gabrieldefreitas2003@gmail.com>
2023-09-05 21:53:15 -0300

    funcoes php

--- funcoes.php
+++ funcoes.php
@@ -20,8 +20,8 @@ subtracao(40,30);
 
 //VALOR DAS VARIAVIES 
 $valor1= 10;
-$valor2 = 10;
-$operador = "**";
+$valor2 = 2;
+$operador = "!!";
 
 //LOGICA DOS OPERADORES
 function  calcular($operador, $valor1, $valor2 ) {
@@ -45,6 +45,34 @@ function  calcular($operador, $valor1, $valor2 ) {
         return exponenciacao($valor1, $valor2); 
     }
 
+    if ($operador == "%" ) {
+        return moduloDivisao($valor1, $valor2); 
+    }
+
+    if ($operador == '^') {
+        return pot($valor1, $valor2);
+    }
+
+    function numeroPar($valor1) {
+        $resto = moduloDivisao($valor1, 2);
+    
+        if ($resto == 0) {
+            $par = 1;
+        } else {
+            $par = 0;
+        }
+    
+        return $par;
+    }
+
+    if ($operador == '!!') {
+        return numeroPar($valor1);
+    }
+    
+    if ($operador == '@@') {
+        return numeroAnoBissextop($valor1);
+    }
+    
 }
 
 //LOGICA DO CALCULO
@@ -71,12 +99,59 @@ function  exponenciacao($valor1, $valor2 ) {
     return $valor1 ** $valor2;
 }
 
+function  moduloDivisao($valor1, $valor2 ) {        
+    return $valor1 % $valor2;
+}
+
+function pot($valor1, $valor2) {
+
+    return pow($valor1, $valor2);
+}
+
+function numeroAnoBissextop($valor1) {
+    // Logica para calcular se o ano($valor1) é bissexto
+    
+}
+
+
 
 //LOGICA DO RESULTADO
 function exibirResultado($resultado) {
     echo $resultado . "<br>";
 }
 
-$resultado = calcular($operador, $valor1, $valor2);
 
 exibirResultado($resultado);
+
+function exibirResultadoLogico($resultado, $valor1) {
+    
+    if ($resultado == 1) {
+        $situacao = "par";
+    } else {
+        $situacao = "impar";
+    }
+
+    echo "O numero informado({$valor1}) é {$situacao}" . "<br>";
+}
+
+function verificarTipoOperador($operador, $resultado, $valor1) {
+        /*
+        '!!' = verificar par
+        '##' = verifica primos
+        '@@' = verifica se o $valor1 se ele é um ano bissexto.
+    */ 
+    $operadoresLogicos = [
+        '!!',
+        '##',
+        '@@'
+    ];
+
+    if (in_array($operador, $operadoresLogicos)) {
+        return exibirResultadoLogico($resultado, $valor1);
+    } else {
+        return exibirResultado($resultado);
+    }
+}
+
+$resultado = calcular($operador, $valor1, $valor2);
+echo verificarTipoOperador($operador, $resultado, $valor1);
