080dd530c9ab9d21e82c5d24c1994f5bb53e1235
argonauta <gabrieldefreitas2003@gmail.com>
2023-09-12 17:58:29 -0300

    fatorial

--- fatorial.php
+++ fatorial.php
@@ -5,7 +5,7 @@
     dica: utilizar laco de repeticao
     n!=n⋅(n−1)⋅(n−2)⋅…⋅3⋅2⋅1
 
-
+*/
 
 $num = 6;
 $n = 1;
@@ -18,24 +18,7 @@ while ($num > 1) {  //numero 6 e maior que 1
 echo "{$n} <br>";
 echo "<br>";
 
-*/
 
 
-function isBissexto($ano) {
-    if (($ano % 4 == 0 && $ano % 100 != 0) || ($ano % 400 == 0)) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
-// Testando alguns anos
-$ano1 = 2015;
-
-
-if (isBissexto($ano1)) {
-    echo "$ano1 é um ano bissexto.<br>";
-} else {
-    echo "$ano1 não é um ano bissexto.<br>";
-}
 
