e3df020d8696c647ab8730060292d264851263f9
gabrielfnaut <gabrieldefreitas2003@gmail.com>
2023-09-26 19:52:06 -0300

    ordenacao

--- ordenacao.php
+++ ordenacao.php
@@ -3,8 +3,8 @@
 //Exibir esse valor ao final para o usuario 
 
 
-/*
-$nums = [500, 1000, 17, 1117, 100, 32732, 1]; //array de numeros 
+
+$nums = [500, 999, 17, 7, 100, 32732, 1, 77, 54, 277]; //array de numeros 
 $max = $nums[0];       //inicializa o maior numero com o primeiro elemento da array, ou seja: 500
 $min = $nums[0];
 
@@ -14,15 +14,18 @@ for ($i= 0; $i < count($nums); $i++ ) {  //$i equivale a 0; $i for menor que (co
     if ($nums[$i] > $max) {     //SE $nums && $i for maior que $max 
         $max = $nums[$i];     //atualiza o maior numero se encontrar um valor maior
     }
+}
 
     //Menor numero ---------------------
-        if ($nums[$i] < $max) {    //SE $nums && $i for menor que $max 
+for ($i= 0; $i < count($nums); $i++ ) {  
+    if ($nums[$i] < $min) {    //SE $nums && $i for menor que $max 
             $min = $nums[$i];     //atualiza o menor numero se encontrar um valor menor
         }
 }
 
-echo " O maior numero da array e: {$max} <br>";
-echo " O menor numero da array e: {$min}";
+echo " O maior numero da array e: {$max} <br> <br>";
+
+echo " O menor numero da array e: {$min} <br> <br>";
 
 
 /*
@@ -37,32 +40,42 @@ for ($i= 0; $i < count($nums); $i++ ) {
     for ($j= 0; $j < count($nums) - 1; $j++) {
         if($nums[$j] > $nums[$j + 1]){
           
-            $aux = $nums[$j];
+            $a = $nums[$j];
             $nums[$j] = $nums[$j + 1];
-            $nums[$j + 1] = $aux;
+            $nums[$j + 1] = $a;
         }
     }
 }
 
 
-echo "array crescente: {$aux} <br>"; 
+echo "array crescente: {$a} <br>"; 
 */
+//--------------------------------------------------------------------------------------------------
+//for | while | foreach | implode, explode || pesquisar sobre oq cada uma faz e para q serve 
+//--------------------------------------------------------------------------------------------------
 
-
-$nums = [500, 999, 17, 7, 100, 32732, 1];
 
 for ($i = 0; $i < count($nums); $i++) {
     for ($j = 0; $j < count($nums) - 1; $j++) {
         if ($nums[$j] > $nums[$j + 1]) {
             
-            $aux = $nums[$j];
+            $a = $nums[$j];                     
             $nums[$j] = $nums[$j + 1];
-            $nums[$j + 1] = $aux;
+            $nums[$j + 1] = $a;
         }
     }
 }
 
 echo "Array ordenado em ordem crescente: ";
 foreach ($nums as $num) {
-    echo $num . " ";
+    echo $num . ", ";
 }
+echo "<br>";
+echo "<br>";
+
+//O echo de cima eh igual ao echo de baixo mas escrito de outra forma 
+
+echo "Array ordenado em ordem crescente: ";
+for ($i = 0; $i < count($nums); $i++) {
+    echo $nums[$i] . ", ";
+} 
